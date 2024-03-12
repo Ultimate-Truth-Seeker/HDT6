149804424cda86ce38f22c8080e8017e0babbe51
Ultimate-Truth-Seeker <96703353+Ultimate-Truth-Seeker@users.noreply.github.com>
2024-03-11 21:41:32 -0600

    Inicio y creación de factory

---
+++ FactoryMap.java
@@ -0,0 +1,18 @@
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.TreeMap;
+
+public class FactoryMap {
+    public <K, V> Map<K, V> getMap(String type) {
+        switch (type) {
+            case "HashMap":
+                return new HashMap<>();
+            case "TreeMap":
+                return new TreeMap<>();
+            case "LinkedHashMap":
+                return new LinkedHashMap<>();
+        }
+        return null;
+    }
+}

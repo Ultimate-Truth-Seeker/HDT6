648d280e59ae84c0997832edfe3dcc8b34684c39
angel <mer23661@uvg.edu.gt>
2024-03-13 16:10:34 -0600

    Opcion 1, 2, 3 y 4 implementados

--- Main.java
+++ Main.java
@@ -1,6 +1,7 @@
 import java.io.File;
 import java.util.Map;
 import java.util.Scanner;
+import java.util.TreeMap;
 
 public class Main {
     public static void main(String[] args) {
@@ -53,12 +54,49 @@ public class Main {
             s.nextLine();
             switch (opcion){
                 case 1:
+                    // Opción 1: Agregar una carta a la colección del usuario
+                    System.out.println("Ingrese el nombre de la carta que desea agregar:");
+                    String cardName = s.nextLine();
+                    if (cards_desc.containsKey(cardName)) {
+                        userCollection.put(cardName, userCollection.getOrDefault(cardName, 0) + 1);
+                        System.out.println("Carta agregada con éxito!");
+                    } 
+                        else {
+                        System.out.println("Error: la carta no se encuentra entre las cartas disponibles.");
+                    }
                     break;
-                case 2:
+                    // Opción 2: Mostrar el tipo de una carta específica
+                    case 2:
+                    System.out.println("Ingrese el nombre de la carta cuyo tipo desea ver:");
+                    cardName = s.nextLine();
+                    if (cards_desc.containsKey(cardName)) {
+                        System.out.println("Tipo de la carta " + cardName + ": " + cards_desc.get(cardName));
+                    } else {
+                        System.out.println("Error: la carta no se encuentra entre las cartas disponibles.");
+                    }
                     break;
                 case 3:
+                    // Opción 3: Mostrar el nombre, tipo y cantidad de cada carta que el usuario tiene en su colección
+                    for (Map.Entry<String, Integer> entry : userCollection.entrySet()) {
+                        System.out.println("Nombre: " + entry.getKey() + ", Tipo: " + cards_desc.get(entry.getKey()) + ", Cantidad: " + entry.getValue());
+                }
                     break;
                 case 4:
+                    // Ordenamos un mapa ordenado por tipo de carta
+                    Map<String, Map<String, Integer>> sortedCollection = new TreeMap<>();
+                    for (Map.Entry<String, Integer> entry : userCollection.entrySet()) {
+                        String cardType = cards_desc.get(entry.getKey());
+                        if (!sortedCollection.containsKey(cardType)) {
+                            sortedCollection.put(cardType, new TreeMap<>());
+                        }
+                    sortedCollection.get(cardType).put(entry.getKey(), entry.getValue());
+                }
+                for (Map.Entry<String, Map<String, Integer>> entry : sortedCollection.entrySet()) {
+                    System.out.println(entry.getKey() + ": ");
+                    for (Map.Entry<String, Integer> cardEntry : entry.getValue().entrySet()) {
+                        System.out.println("Nombre: " + cardEntry.getKey() + ", Cantidad: " + cardEntry.getValue()+ "\n");
+                    }
+                }
                     break;
                 case 5:
                 for (String key : cards_desc.keySet()) {
@@ -90,27 +128,6 @@ public class Main {
         }
 
         s.close();
-
-        // // Opción 5
-        // for (String key : cards_desc.keySet()) {
-        //     System.out.println("Nombre: " + key + "\nTipo:  " + cards_desc.get(key));
-        // }
-
-        // // Opción 6
-        // Map<String, Map<String, String>> tipos = FactoryMap.getMap(type);
-        // for (String card : cards_desc.keySet()) {
-        //     if (!tipos.containsKey(cards_desc.get(card))) {
-        //         tipos.put(cards_desc.get(card), FactoryMap.getMap(type));
-        //     }
-        //     tipos.get(cards_desc.get(card)).put(card, card);
-        // }
-        // for (String tp : tipos.keySet()) {
-        //     System.out.println(tp + ": ");
-        //     for (String cd : tipos.get(tp).keySet()) {
-        //         System.out.println("\t" + cd);
-        //     }
-        // }
-        
        
     }
     

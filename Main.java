aa8110fb79d77c0e00a9ab49eea021dde0504890
Ultimate-Truth-Seeker <96703353+Ultimate-Truth-Seeker@users.noreply.github.com>
2024-03-13 12:19:17 -0600

    Reparación lectura y opciones 5 y 6

--- Main.java
+++ Main.java
@@ -32,8 +32,8 @@ public class Main {
             cards_desc = FactoryMap.getMap(type);
 
             while (sc.hasNextLine()) {
-                String[] pair = sc.nextLine().split("|");
-                cards_desc.put(pair[0], pair[1]);
+                String line = sc.nextLine();
+                cards_desc.put(line.substring(0, line.indexOf("|")), line.substring(line.indexOf("|") + 1));
             }
             
         } catch (Exception e) {
@@ -43,10 +43,28 @@ public class Main {
         }
 
         System.out.println("Archivo cargado con éxito!");
-        Map<String, String> userCollection = FactoryMap.getMap(type); // cartas del usuario
+        Map<String, Integer> userCollection = FactoryMap.getMap(type); // cartas del usuario
 
-        //TODO: implement menu
+        // Opción 5
+        for (String key : cards_desc.keySet()) {
+            System.out.println("Nombre: " + key + "\nTipo:  " + cards_desc.get(key));
+        }
 
+        // Opción 6
+        Map<String, Map<String, String>> tipos = FactoryMap.getMap(type);
+        for (String card : cards_desc.keySet()) {
+            if (!tipos.containsKey(cards_desc.get(card))) {
+                tipos.put(cards_desc.get(card), FactoryMap.getMap(type));
+            }
+            tipos.get(cards_desc.get(card)).put(card, card);
+        }
+        for (String tp : tipos.keySet()) {
+            System.out.println(tp + ": ");
+            for (String cd : tipos.get(tp).keySet()) {
+                System.out.println("\t" + cd);
+            }
+        }
+        
         s.close();
     }
     

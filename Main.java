a76e9290f8ecdc62a5d332b19591054f567e2c1c
angel <mer23661@uvg.edu.gt>
2024-03-13 15:48:29 -0600

    Menu implmentado con opcion 5 y 6

--- Main.java
+++ Main.java
@@ -45,27 +45,73 @@ public class Main {
         System.out.println("Archivo cargado con éxito!");
         Map<String, Integer> userCollection = FactoryMap.getMap(type); // cartas del usuario
 
-        // Opción 5
-        for (String key : cards_desc.keySet()) {
-            System.out.println("Nombre: " + key + "\nTipo:  " + cards_desc.get(key));
+        //Implementacion del menu:
+        boolean exit = false;
+        while (!exit){
+            System.out.println("Seleccione una opcion:\n1.Agregar una carta a la colección\n2.Mostrar el tipo de una carta específica\n3.Mostrar la colección del usuario\n4.Mostrar la colección del usuario ordenada por tipo\n5.Mostrar todas las cartas existentes\n6.Mostrar todas las cartas existentes ordenadas por tipo\n7.Salir");
+            int opcion = s.nextInt();
+            s.nextLine();
+            switch (opcion){
+                case 1:
+                    break;
+                case 2:
+                    break;
+                case 3:
+                    break;
+                case 4:
+                    break;
+                case 5:
+                for (String key : cards_desc.keySet()) {
+                    System.out.println("Nombre: " + key + "\nTipo:  " + cards_desc.get(key));
+                }
+                    break;
+                case 6:
+                Map<String, Map<String, String>> tipos = FactoryMap.getMap(type);
+                for (String card : cards_desc.keySet()) {
+                    if (!tipos.containsKey(cards_desc.get(card))) {
+                        tipos.put(cards_desc.get(card), FactoryMap.getMap(type));
+                    }
+                    tipos.get(cards_desc.get(card)).put(card, card);
+                }
+                for (String tp : tipos.keySet()) {
+                    System.out.println(tp + ": ");
+                    for (String cd : tipos.get(tp).keySet()) {
+                        System.out.println("\t" + cd);
+                    }
+                }
+                    break;
+                case 7:
+                    exit = true;
+                    break;
+                default:
+                    System.out.println("Opcion no existente, selecciona otra opcion");
+                    break;
+            }
         }
 
-        // Opción 6
-        Map<String, Map<String, String>> tipos = FactoryMap.getMap(type);
-        for (String card : cards_desc.keySet()) {
-            if (!tipos.containsKey(cards_desc.get(card))) {
-                tipos.put(cards_desc.get(card), FactoryMap.getMap(type));
-            }
-            tipos.get(cards_desc.get(card)).put(card, card);
-        }
-        for (String tp : tipos.keySet()) {
-            System.out.println(tp + ": ");
-            for (String cd : tipos.get(tp).keySet()) {
-                System.out.println("\t" + cd);
-            }
-        }
-        
         s.close();
+
+        // // Opción 5
+        // for (String key : cards_desc.keySet()) {
+        //     System.out.println("Nombre: " + key + "\nTipo:  " + cards_desc.get(key));
+        // }
+
+        // // Opción 6
+        // Map<String, Map<String, String>> tipos = FactoryMap.getMap(type);
+        // for (String card : cards_desc.keySet()) {
+        //     if (!tipos.containsKey(cards_desc.get(card))) {
+        //         tipos.put(cards_desc.get(card), FactoryMap.getMap(type));
+        //     }
+        //     tipos.get(cards_desc.get(card)).put(card, card);
+        // }
+        // for (String tp : tipos.keySet()) {
+        //     System.out.println(tp + ": ");
+        //     for (String cd : tipos.get(tp).keySet()) {
+        //         System.out.println("\t" + cd);
+        //     }
+        // }
+        
+       
     }
     
 }
